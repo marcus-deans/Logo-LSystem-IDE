c987b2df821b2f362de7c9ba1aa60195418efd32
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2021-10-08 15:05:52 -0400

    Added geometrical calculation to determine if there is another entity within the field of view of the creature we are referencing

--- src/oolala/model/commands/conditionals/ConditionalCommand.java
+++ src/oolala/model/commands/conditionals/ConditionalCommand.java
@@ -8,12 +8,19 @@ public abstract class ConditionalCommand extends ModelCommand {
   protected int myNextCommand;
   protected ModelCreature myModelCreature;
   protected int myNearbyThreshold;
+  protected int myModelCreatureX;
+  protected int myModelCreatureY;
+  protected int myDegreesRotation;
+  protected static final int FULL_CIRCLE = 360;
 
   public ConditionalCommand(ModelCreature modelCreature, int nextCommand) {
     super(modelCreature);
     myModelCreature = modelCreature;
     myNextCommand = nextCommand;
     myNearbyThreshold = myModelCreature.getMyNearbyThreshold();
+    myModelCreatureX = myModelCreature.getTurtleCoordinates().turtleNewX;
+    myModelCreatureY = myModelCreature.getTurtleCoordinates().turtleNewY;
+    myDegreesRotation = myModelCreature.getDegreesRotation();
   }
 
   protected void executeSpecifiedInstruction() {
