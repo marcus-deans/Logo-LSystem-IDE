1b816c976626f8be57eafff5fbb410249e7037cb
Marcus Deans <marcus.deans@duke.edu>
2021-10-11 19:11:36 -0400

    Fully commented instruction classes

--- src/oolala/model/instructions/LSystemInstruction.java
+++ src/oolala/model/instructions/LSystemInstruction.java
@@ -1,14 +1,36 @@
 package oolala.model.instructions;
 
-public class LSystemInstruction extends Instruction{
+/**
+ * @author marcusdeans
+ * <p>
+ * Purpose: Create an instruction for the turtle for perform an LSystem action Assumptions:
+ * Functioning ModerlTurtle; other values error-checked Dependencies: Instruction Example Usage:
+ * used for any LSystem command User Details: None, usage exactly as dictated by command entry
+ * pattern (error-checked)
+ */
+public class LSystemInstruction extends Instruction {
+
   public int level;
 
-  public LSystemInstruction(int inLevel, String inCommand){
+  /**
+   * Create a new LSystem instruction
+   *
+   * @param inLevel   level of the fractal
+   * @param inCommand string of command name
+   */
+  public LSystemInstruction(int inLevel, String inCommand) {
     super(inCommand);
     level = inLevel;
   }
 
-  public LSystemInstruction(int inLevel, String inCommand, int inPixels){
+  /**
+   * Create a new LSystem instruction
+   *
+   * @param inLevel   level of the fractal
+   * @param inCommand string of command name
+   * @param inPixels  amount of pixels for movement or degrees for rotation
+   */
+  public LSystemInstruction(int inLevel, String inCommand, int inPixels) {
     super(inCommand, inPixels);
     level = inLevel;
   }
