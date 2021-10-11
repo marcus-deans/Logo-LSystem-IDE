fe707ba872cfc6016b31de829d005196167b2198
Marcus Deans <marcus.deans@duke.edu>
2021-10-11 13:05:49 -0400

    Creating proper Darwin execution procedure

--- src/oolala/view/displays/DarwinDisplay.java
+++ src/oolala/view/displays/DarwinDisplay.java
@@ -2,29 +2,80 @@ package oolala.view.displays;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Random;
+import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
 import oolala.model.ModelCreature;
+import oolala.model.instructions.CreatureInstruction;
+import oolala.model.processors.Darwin;
 
-public class DarwinDisplay extends Display{
+public class DarwinDisplay extends Display {
+
   private static ArrayList<ModelCreature> allModelCreatures;
 
-  public DarwinDisplay(){
-    super();
+  @Override
+  protected Scene setupGame(int width, int height, Paint background) {
+    myGameProcessor = new Darwin();
     allModelCreatures = new ArrayList<>();
+    performInitialSetup();
+    initializeRunButton(); //initialize the program run button
+    //Set the scene
+    Scene scene = new Scene(root, width, height, background);
+    scene.getStylesheets().add(LogoDisplay.class.getResource("Display.css").toExternalForm());
+    return scene;
+
   }
-  public static ArrayList<ModelCreature> getAllModelCreatures(){
+
+  public static ArrayList<ModelCreature> getAllModelCreatures() {
     return allModelCreatures;
   }
 
-
-
   @Override
   protected void handleInputParsing(String text) {
-    //TODO: send stuff to input parser
+    myGameProcessor.inputParser(4, 4, 4, text);
   }
+
+  //Create method that passes in queue of commands to Logo
+  @Override
+  protected void step() {
+    checkForInstructionsAndExecute();
+  }
+
+  private void checkForInstructionsAndExecute() {
+    LinkedList<ArrayList<CreatureInstruction>> creatureExecutionOrderList = randomizeCreatureOrder();
+    //TODO: execute all instructions
+  }
+
+  private LinkedList<ArrayList<CreatureInstruction>> randomizeCreatureOrder() {
+    //TODO: get instructions and execute in random order
+    Random rand = new Random();
+    // create a temporary list for storing
+    // selected element
+    LinkedList<ArrayList<CreatureInstruction>> randomizedOptionOrder = new ArrayList<>();
+    ArrayList<ArrayList<CreatureInstruction>> allCreaturesInstructions = myGameProcessor.getCreatureList;
+    for (int i = 0; i < allCreaturesInstructions.size(); i++) {
+
+      // take a random index between 0 to size
+      // of given List
+      int randomIndex = rand.nextInt(allCreaturesInstructions.size());
+
+      // add element in temporary list
+      randomizedOptionOrder.add(allCreaturesInstructions.get(randomIndex));
+
+      // Remove selected element from original list
+      allCreaturesInstructions.remove(randomIndex);
+    }
+    return randomizedOptionOrder;
+//    ArrayList<Integer> numericOptions = createNumericOptionsArraylist();
+  }
+
+//  private ArrayList<Integer> createNumericOptionsArraylist (ArrayList<Integer> list)
+
 
   @Override
   protected File[] getFilesFromPath() {
     return new File("data/examples/darwin").listFiles();
   }
+
 }
