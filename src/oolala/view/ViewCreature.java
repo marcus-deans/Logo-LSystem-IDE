d2fe7fb446234c31055b3a08c126239a40cca325
Marcus Deans <marcus.deans@duke.edu>
2021-10-12 00:31:41 -0400

    Additional commentary for TurtleLinkage and ViewCreature

--- src/oolala/view/ViewCreature.java
+++ src/oolala/view/ViewCreature.java
@@ -1,8 +1,20 @@
 package oolala.view;
 
-public class ViewCreature extends ViewTurtle{
+/**
+ * @author marcusdeans
+ * <p>
+ * Purpose: Create a view of the creature that will move around on the screen Assumptions:
+ * Functioning coordinate system and commmands Dependencies: ViewTurtle Example Usage: Used in Logo
+ * and LSystem programs to create the visual object that moves on the screen Details: None, usage
+ * exactly as dictated by command entry pattern (error-checked)
+ */
+public class ViewCreature extends ViewTurtle {
 
-
+  /**
+   * Create new ViewCreature object
+   *
+   * @param id unique identifier for ViewCreature
+   */
   public ViewCreature(int id) {
     super(id);
   }
